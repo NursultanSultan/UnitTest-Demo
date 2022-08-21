7a104a863240c1e18a628b8ecdaccebbf469ae84
NursultanSultan <tu201906251@code.edu.az>
2022-08-21 13:38:12 +0400

    feat: added summary

--- UnitTestProject/ValuesTest.cs
+++ UnitTestProject/ValuesTest.cs
@@ -2,6 +2,15 @@ using UnitTestingDemo.Controllers;
 
 namespace UnitTestProject
 {
+    
+    /// <summary>
+    /// * IClassFixture inherit etmeyimizin sebebi odur ki xUnit.net her Unit test ucun 
+    /// olduqu sinifin ferqli bir instancesin yaradir buda uretim maliyyetini artirir 
+    /// bu maliyyeti duwurmey ucun IClassFixturedan miras aliriq
+    /// 
+    /// 
+    /// * Testlerin Paralel iwleye bilmesi ucun heresini ayri bir classda yazmaq lazimdir 
+    /// </summary>
     public class ValuesTest : IClassFixture<ItemsController>
     {
         ItemsController _itemsController;
@@ -10,7 +19,11 @@ namespace UnitTestProject
             _itemsController = new();
         }
 
-
+        /// <summary>
+        /// Test muddetinde methodumuz parametr qebul etmirse Fact keyword ile iwaretleyirik 
+        /// bunun bize plus odurki TestExplorer Fact ile isaretlenmis methodlarin 
+        /// Test methodlari olduqunu anlayir ve Assert e gore bize testin neticesin gosterir.
+        /// </summary>
         [Fact]
         public void GetTest()
         {
@@ -26,6 +39,12 @@ namespace UnitTestProject
 
         }
 
+        /// <summary>
+        /// Test muddetinde methodumuz parametr qebul edirse Theory keyword ile iwaretleyirik
+        /// InlineData keyword istifade ederek methoda parametrimizi oture bilerik 
+        /// Test methoduna gondereceyimiz parametrler cox olduqda MemberData istifade olunur
+        /// MemberData atributuna alternatif olaraq ClassData da istifade olunur
+        /// </summary>
         [Theory]
         [InlineData(2)]
         public void GetCountTest(int count)
